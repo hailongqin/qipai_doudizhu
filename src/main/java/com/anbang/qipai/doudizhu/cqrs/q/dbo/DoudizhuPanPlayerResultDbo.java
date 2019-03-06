7bcd9c433885480f02e4ae4a5d36b6674afef177
林少聪 < @PC-20180515PRDG>
2019-03-06 17:43:49 +0800

    developemt

---
+++ src/main/java/com/anbang/qipai/doudizhu/cqrs/q/dbo/DoudizhuPanPlayerResultDbo.java
@@ -0,0 +1,10 @@
+package com.anbang.qipai.doudizhu.cqrs.q.dbo;
+
+import com.dml.doudizhu.player.DoudizhuPlayerValueObject;
+
+public class DoudizhuPanPlayerResultDbo {
+
+	private String playerId;
+	private DoudizhuPlayerValueObject player;
+
+}
