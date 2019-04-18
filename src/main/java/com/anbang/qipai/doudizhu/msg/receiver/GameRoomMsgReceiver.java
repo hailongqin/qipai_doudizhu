f13301ab9dd9907bbbcbd943e2131ae19b44527c
林少聪 < @PC-20180515PRDG>
2019-04-18 16:57:57 +0800

    bugfix

--- src/main/java/com/anbang/qipai/doudizhu/msg/receiver/GameRoomMsgReceiver.java
+++ src/main/java/com/anbang/qipai/doudizhu/msg/receiver/GameRoomMsgReceiver.java
@@ -62,10 +62,12 @@ public class GameRoomMsgReceiver {
 					} else {
 						PukeGameValueObject gameValueObject = gameCmdService.finishGameImmediately(gameId);
 						pukeGameQueryService.finishGameImmediately(gameValueObject);
-						JuResultDbo juResultDbo = pukePlayQueryService.findJuResultDbo(gameId);
-						PukeHistoricalJuResult juResult = new PukeHistoricalJuResult(juResultDbo, pukeGameDbo);
-						doudizhuResultMsgService.recordJuResult(juResult);
 						doudizhuGameMsgService.gameFinished(gameId);
+						JuResultDbo juResultDbo = pukePlayQueryService.findJuResultDbo(gameId);
+						if (juResultDbo != null) {
+							PukeHistoricalJuResult juResult = new PukeHistoricalJuResult(juResultDbo, pukeGameDbo);
+							doudizhuResultMsgService.recordJuResult(juResult);
+						}
 					}
 				} catch (Throwable e) {
 					e.printStackTrace();
